7d2f46c89205cc6b0fc2917d375e802557b0a0e4
DoE <34648417+eldar6776@users.noreply.github.com>
2018-05-17 00:02:03 +0200

    Add files via upload

---
+++ fw/Bootloader/Project/RTE/RTE_Components.h
@@ -0,0 +1,17 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: DE-200816
+ * RTE configuration: DE-200816.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
